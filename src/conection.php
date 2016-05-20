5b3c0efc9f6e424899d7a593c40d3d69b8e8bb4d
madb88 <kaminskiqba@gmail.com>
2016-05-20 12:05:22 +0200

    Dodaje folder src

---
+++ src/conection.php
@@ -0,0 +1,18 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "coderslab";
+$baseName = "Twitter";
+
+//Tworzenie nowego polaczenia 
+$conn = new mysqli($servername, $username, $password, $baseName);
+
+//Sprawdzamy czy polaczenie sie udalo
+if($conn->connect_error){
+    die("Polaczenie nieudane. Blad: ".$conn->connect_error);
+    } else {
+    echo "Polaczenie udane"."<br>";
+}
+
+
